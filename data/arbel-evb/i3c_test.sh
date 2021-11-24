81c8bb7bb8944bd122a4670a1f7a1f8e1a4dbca7
Stanley Chu <yschu@nuvoton.com>
2021-11-24 16:36:08 +0800

    arbel-evb: update test script for I3C/PSPI
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- data/arbel-evb/i3c_test.sh
+++ data/arbel-evb/i3c_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+log_file="/tmp/i3c_test.log"
 
 # attach LSM6DSO to i3c bus#1
 
@@ -8,12 +9,12 @@ sleep 1
 echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/bind
 sleep 1
 
-echo "check I3C pid sysfs exist"
+echo "check I3C pid sysfs exist" > $log_file
 pid=`cat /sys/bus/i3c/devices/1-208006c100b/pid`
 
-echo "check pid value is correct"
-echo "pid=$pid"
+echo "check pid value is correct" >> $log_file
+echo "pid=$pid" >> $log_file
 test $pid == 208006c100b
 
-echo "PASS"
+echo "PASS" >> $log_file
 exit 0
