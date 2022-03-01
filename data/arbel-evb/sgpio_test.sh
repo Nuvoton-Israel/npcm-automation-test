005086d21d2d61741e2dd751fb9d9f5b9a4655bb
Medad CChien <ctcchien@nuvoton.com>
2022-03-01 11:30:35 +0800

    arbel-evb: sgpio: Skip 6,7,70,71  which are dedicated for NMI
    Signed-off-by: Medad CChien <ctcchien@nuvoton.com>

--- data/arbel-evb/sgpio_test.sh
+++ data/arbel-evb/sgpio_test.sh
@@ -4,25 +4,27 @@ set -e
 for ((i=1;i<=10;i++))
 do
 	log_file="/tmp/log/sgpio_test.$i.log"
-	gpiomon -n 24 8 64 65 70 71 72 73 74 75 76 77 78 79  > $log_file &
+	gpiomon -n 20 8 64 65 72 73 74 75 76 77 78 79  > $log_file &
 
 	t=0
-	while [ "$t" != "13" ]
+	while [ "$t" != "11" ]
 	do
 		t=$(ps |  grep  "gpiomon]" | wc -l)
 	done
 
-	gpioset 8 0=1 1=1 6=1 7=1 8=1 9=1 10=1 11=1 12=1 13=1 14=1 15=1
-	gpioset 8 0=0 1=0 6=0 7=0 8=0 9=0 10=0 11=0 12=0 13=0 14=0 15=0
+	gpioset 8 0=1 1=1 8=1 9=1 10=1 11=1 12=1 13=1 14=1 15=1
+	gpioset 8 0=0 1=0 8=0 9=0 10=0 11=0 12=0 13=0 14=0 15=0
 
-	Pin_Count=$(awk 'BEGIN{x=0;y=0}{ y++ ; x+=$5 ; if(y==24) print x ;}' $log_file)
+	Pin_Count=$(awk 'BEGIN{x=0;y=0}{ y++ ; x+=$5 ; if(y==20) print x ;}' $log_file)
 	echo $i:$Pin_Count
-	if [ "$Pin_Count" != "1748" ]
+	if [ "$Pin_Count" != "1466" ]
 	then
 		echo FAIL  >> $log_file
+		echo FAIL
 		exit 1
 	else
 		echo PASS  >> $log_file
+		echo PASS
 	fi
 
 	t=2
