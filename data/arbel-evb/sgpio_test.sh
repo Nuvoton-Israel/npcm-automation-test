e57afb7ae74083e5306036c31c07811948dd5e03
Medad CChien <ctcchien@nuvoton.com>
2022-03-23 17:18:06 +0800

    arbel-evb: Update SGPIO script
    Use one gpiomon for all the pins,
    instead of ten gpiomons for ten pins
    
    Signed-off-by: Medad CChien <ctcchien@nuvoton.com>

--- data/arbel-evb/sgpio_test.sh
+++ data/arbel-evb/sgpio_test.sh
@@ -5,17 +5,7 @@ for ((i=1;i<=10;i++))
 do
 	gpioset 8 0=0 1=0 8=0 9=0 10=0 11=0 12=0 13=0 14=0 15=0
 	log_file="/tmp/log/sgpio_test.$i.log"
-	rm -f $log_file
-	gpiomon -n 2 8 64 >> $log_file &
-	gpiomon -n 2 8 65 >> $log_file &
-	gpiomon -n 2 8 72 >> $log_file &
-	gpiomon -n 2 8 73 >> $log_file &
-	gpiomon -n 2 8 74 >> $log_file &
-	gpiomon -n 2 8 75 >> $log_file &
-	gpiomon -n 2 8 76 >> $log_file &
-	gpiomon -n 2 8 77 >> $log_file &
-	gpiomon -n 2 8 78 >> $log_file &
-	gpiomon -n 2 8 79 >> $log_file &
+	gpiomon -n 20 8 64 65 72 73 74 75 76 77 78 79  > $log_file &
 
 	t=0
 	while [ "$t" != "11" ]
@@ -28,12 +18,11 @@ do
 
 	t=11
 	c=10
-        while [ "$t" != "1" ] &&  [ "$c" != "0" ]
-        do
+	while [ "$t" != "1" ] &&  [ "$c" != "0" ]
+	do
 		(( c-- ))
-                t=$(ps |  grep  "gpiomon]" | wc -l)
-		ps | grep "gpiomon -n 2 8" >> $log_file
-        done
+		t=$(ps |  grep  "gpiomon]" | wc -l)
+	done
 
 	if [ "$t" != "1" ]
 	then
