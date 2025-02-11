39d6652be72d17ff9bf53118619b52e1c560ddff
Eason Yang <j2anfernee@gmail.com>
2025-02-11 16:03:40 +0800

    arbel-evb: update test script for I3C
    attach NPCM400F EVB I3C 6 to NPCM845 EVB i3c bus#0
    
    Signed-off-by: Eason Yang <j2anfernee@gmail.com>

--- data/arbel-evb/i3c_test.sh
+++ data/arbel-evb/i3c_test.sh
@@ -2,7 +2,7 @@
 set -e
 log_file="/tmp/log/i3c_test.log"
 
-# attach LSM6DSO to i3c bus#1
+# attach OpenBIC to i3c bus#0
 
 echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/unbind
 sleep 1
@@ -10,11 +10,11 @@ echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/bind
 sleep 1
 
 echo "check I3C pid sysfs exist" > $log_file
-pid=`cat /sys/bus/i3c/devices/1-208006c100b/pid`
+pid=`cat /sys/bus/i3c/devices/0-63212345607/pid`
 
 echo "check pid value is correct" >> $log_file
 echo "pid=$pid" >> $log_file
-test $pid == 208006c100b
+test $pid == 63212345607
 
 echo "PASS" >> $log_file
 echo "PASS"
